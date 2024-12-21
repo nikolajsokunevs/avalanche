37912dbd929b75a065d06cb46ee024fb4cdb9040
nikolajs.okunevs <nikolajs.okunevs@agcsoft.com>
2024-12-22 00:32:17 +0200

    ON: add ws

--- src/main/java/lv/on/avalanche/config/WebSecurityConfig.java
+++ src/main/java/lv/on/avalanche/config/WebSecurityConfig.java
@@ -16,11 +16,15 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOriginPatterns(Arrays.asList(
+                "http://localhost:4200",
+                "https://avalanche-game-tg-app.web.app" // Add your production URL here
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
