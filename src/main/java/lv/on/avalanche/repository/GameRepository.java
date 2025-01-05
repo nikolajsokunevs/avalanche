d2cc006fd221fb3f0d1f3c8863ad54aa939a4b8f
nikolajs.okunevs <nikolajs.okunevs@agcsoft.com>
2025-01-05 22:48:54 +0200

    ON: add additional endpoint

--- src/main/java/lv/on/avalanche/repository/GameRepository.java
+++ src/main/java/lv/on/avalanche/repository/GameRepository.java
@@ -1,7 +1,17 @@
 package lv.on.avalanche.repository;
 
 import lv.on.avalanche.entities.GameEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface GameRepository extends CrudRepository<GameEntity, Long> {
+
+    @Query("SELECT g FROM GameEntity g WHERE g.user1Id = :userId OR g.user2Id = :userId")
+    List<GameEntity> findAllByUser(@Param("userId") Long userId);
+
+    @Query("SELECT g FROM GameEntity g WHERE g.inProgress = false AND (g.user1Id = :userId OR g.user2Id = :userId)")
+    List<GameEntity> findByInProgressFalseAndUser1IdOrUser2Id(Long userId);
 }
